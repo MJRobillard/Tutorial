7da7850416db721ebc6410ef95a7dcaa51a0b462
Matthew Robillard <robillard.matthew22@gmail.com>
2024-02-26 20:28:10 -0800

    2/26/2024

---
+++ jsDOM/inClass/index.js
@@ -0,0 +1,603 @@
+var exesList = [
+    {
+      "name": "Grace Moore",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "David Moore",
+      "grade": "Sophomore",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Bob Williams",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Grace Miller",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Jones",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Taylor",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Bob Jones",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Grace Johnson",
+      "grade": "Sophomore",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Frank Moore",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Bob Williams",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Grace Miller",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Alice Smith",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Grace Wilson",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Alice Miller",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Moore",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Jane Taylor",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Davis",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Isaac Davis",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Charlie Jones",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Grace Miller",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Eva Miller",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Isaac Miller",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Jane Moore",
+      "grade": "Sophomore",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Isaac Davis",
+      "grade": "Sophomore",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Eva Taylor",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Hannah Taylor",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Frank Taylor",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Grace Wilson",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Eva Smith",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Isaac Taylor",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Wilson",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Jane Jones",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Miller",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "David Smith",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Eva Jones",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Alice Williams",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Bob Wilson",
+      "grade": "Sophomore",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Moore",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "David Johnson",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Wilson",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Eva Brown",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Jane Johnson",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Isaac Jones",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Eva Miller",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Alice Davis",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Miller",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Hannah Davis",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Frank Jones",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "David Smith",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Johnson",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Brown",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Eva Wilson",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Jane Moore",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Bob Williams",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Charlie Johnson",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Bob Williams",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Isaac Davis",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Isaac Moore",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Jane Davis",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Miller",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Frank Johnson",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Jones",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Charlie Williams",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Frank Smith",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "David Smith",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Frank Jones",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Moore",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Bob Johnson",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Jones",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Taylor",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Charlie Taylor",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Davis",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Hannah Brown",
+      "grade": "Junior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Isaac Brown",
+      "grade": "Sophomore",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Jane Moore",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Frank Wilson",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Sophomore",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Hannah Jones",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Frank Moore",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Alice Miller",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Isaac Smith",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Bob Smith",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Jane Williams",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Isaac Jones",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Isaac Brown",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Isaac Miller",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Grace Smith",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Eva Davis",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Alice Williams",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Charlie Moore",
+      "grade": "Senior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Bob Miller",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Is a bear"
+    },
+    {
+      "name": "Charlie Wilson",
+      "grade": "Junior",
+      "favoriteFood": "Mexican",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Charlie Johnson",
+      "grade": "Senior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Never called"
+    },
+    {
+      "name": "Isaac Davis",
+      "grade": "Junior",
+      "favoriteFood": "Pizza",
+      "breakupReason": "Talking to too many girls"
+    },
+    {
+      "name": "Bob Wilson",
+      "grade": "Senior",
+      "favoriteFood": "Sushi",
+      "breakupReason": "Is a bear"
+    }
+  ];
+
