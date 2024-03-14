2d5531104feaa03e7c694db9c9977e88bb55ab29
Matthew Robillard <robillard.matthew22@gmail.com>
2024-03-13 17:54:35 -0700

    reactPart

---
+++ react3tutorial/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import './App.css';
+import {  StyledContainer, StyledH3  } from './customeStyles.js';
+
+
+function App() {
+  return (
+    <StyledContainer>
+      <StyledH3>Hi There!</StyledH3>
+    </StyledContainer>
+  );
+}
+
+export default App;
+
+
---
+++ react3tutorial/src/customeStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+
+const StyledContainer = styled.div`
+  margin-left: 2rem;
+`;
+
+const StyledH3 = styled.h3`
+  color: red;
+  font-size: larger;
+`;
+export { StyledContainer, StyledH3 };
